2fe831e0f94b3dd8a73abefe114d7fdf4403b21c
baraksu <baraksu@gmail.com>
2026-01-13 23:18:39 +0200

    Sync HotelRoomYonathanklein2010droidTester.java from maman02-yonathanklein2010-droid

---
+++ HotelRoomYonathanklein2010droidTester.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HotelRoomYonathanklein2010droidTester {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
+
